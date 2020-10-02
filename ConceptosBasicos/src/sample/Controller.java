4458f4b996548cf3de44b901dc51f0ddcdf18d2c
Shaggy23-cell <71805009+Shaggy23-cell@users.noreply.github.com>
2020-10-02 12:30:12 -0600

    creacion de libreria

--- ConceptosBasicos/src/sample/Controller.java
+++ ConceptosBasicos/src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import conversiones.Conversion;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -10,7 +11,9 @@ public class Controller {
     @FXML Label lblTitulo;
 
     public void evento1(ActionEvent event){
-        int dias= Integer.parseInt(txtEdad.getText())*365;
-        lblTitulo.setText("Tu has vivido "+dias+" dias");
+        Conversion td= new Conversion();
+        int numero= Integer.parseInt(txtEdad.getText());
+        String res= td.binario(numero);
+        lblTitulo.setText(res);
     }
 }
